de7edaff0b9e72f5073e367ae2a287ff7f3e523a
Santiago Uria <uriasantiago.a@gmail.com>
2020-07-27 10:22:29 -0300

    arraymarcas

--- Controller/marcasController.js
+++ Controller/marcasController.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
+const { kMaxLength } = require('buffer');
 const concesionarias = JSON.parse(fs.readFileSync('./data/concesionarias.json', 'utf-8'));
 let marcasController ={
     home : (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
-    res.write(concesionarias[i].autos[marcas])
-    res.end()
+    concesionarias.forEach(concesionaria=>{
+           for (let i = 0; i > array.length; i++) {
+               const arrayMarcas = arrayMarcas.push(concesionaria.autos[i])
+               
+               return res.write(arrayMarcas);
+           }
+    }); res.end();
+        
 },
  marca : (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
@@ -12,7 +19,6 @@ let marcasController ={
 res.send();
 res.end();        
 }
-}
-;
+};
 
 module.exports = marcasController
